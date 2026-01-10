a1e4302e2bfef23cb7ef68488e7c1abab0c281c7
abhijithms1213 <abhijithms1213@gmail.com>
2026-01-10 16:35:25 +0530

    added if condition to check default wallpaper and set it else theme based

--- allThemes/wallpaper_switcher.sh
+++ allThemes/wallpaper_switcher.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-swww img -o eDP-1 ~/Downloads/Wallpapers/theme_based/$1 -t random
-swww img -o HDMI-A-1 ~/Downloads/Wallpapers/theme_based/$2 -t random
+if [ "$1" == "$HOME/.cache/current_wallpaper" ] && [ "$2" == "$HOME/.cache/current_wallpaper_hdmi" ]; then
+  wall_eDP=$(cat ~/.cache/current_wallpaper)
+  wall_HDMI=$(cat ~/.cache/current_wallpaper_hdmi)
+  # echo "default if worked from wallpaper switcher"
+  swww img -o eDP-1 "$wall_eDP" -t random
+  swww img -o HDMI-A-1 "$wall_HDMI" -t random
+else
+  # echo "else worked from wallpaper switcher"
+  swww img -o eDP-1 ~/Downloads/Wallpapers/theme_based/$1 -t random
+  swww img -o HDMI-A-1 ~/Downloads/Wallpapers/theme_based/$2 -t random
+fi
