25cc6a15025d58e4f00cdc333ee2b27b79115376
abhijithms1213 <abhijithms1213@gmail.com>
2026-01-14 12:22:43 +0530

    changed duration apply to the wallpapers

--- allThemes/wallpaper_switcher.sh
+++ allThemes/wallpaper_switcher.sh
@@ -4,10 +4,12 @@ if [ "$1" == "$HOME/.cache/current_wallpaper" ] && [ "$2" == "$HOME/.cache/curre
   wall_eDP=$(cat ~/.cache/current_wallpaper)
   wall_HDMI=$(cat ~/.cache/current_wallpaper_hdmi)
   # echo "default if worked from wallpaper switcher"
-  swww img -o eDP-1 "$wall_eDP" -t random
   swww img -o HDMI-A-1 "$wall_HDMI" -t random
+  sleep 4
+  swww img -o eDP-1 "$wall_eDP" -t random
 else
   # echo "else worked from wallpaper switcher"
-  swww img -o eDP-1 ~/Downloads/Wallpapers/theme_based/$1 -t random
   swww img -o HDMI-A-1 ~/Downloads/Wallpapers/theme_based/$2 -t random
+  sleep 4
+  swww img -o eDP-1 ~/Downloads/Wallpapers/theme_based/$1 -t random
 fi
